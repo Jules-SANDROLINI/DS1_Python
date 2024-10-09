0ee8f19d77224b7f7999b2aa066488f8fd89c8f9
Jules-SANDROLINI <jules.sandrolini@gmail.com>
2024-10-09 17:51:50 +0200

    Update comptage_de_voyelle.py

--- comptage_de_voyelle.py
+++ comptage_de_voyelle.py
@@ -4,5 +4,5 @@ def compte_voyelles(chaine):
         if char in "aeiouyAEIOUYéèàêôù":
            nbr_voyelle = nbr_voyelle+1
     return nbr_voyelle
-chaine = input('Enter any string: ')
-print('nobre de voyelle =',compte_voyelles(chaine))
+chaine = input('saisir une phrase: ')
+print('nombre de voyelle =',compte_voyelles(chaine))
